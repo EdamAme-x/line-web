5c67f6718de3fca8d473a0996a90177842bd59c8
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-08-26 19:01:08 +0900

    update

--- index.ts
+++ index.ts
@@ -4,62 +4,75 @@ import { serveStatic } from "@hono/hono/deno";
 const app = new Hono();
 
 app.all("/_proxy/R4", async (c) => {
-    const query = (new URL(c.req.url).searchParams.toString());
-    const targetURL = "https://ci.line-apps.com/R4" + (query === "" ? "" : "?" + query);
+  const query = new URL(c.req.url).searchParams.toString();
+  const targetURL = "https://ci.line-apps.com/R4" +
+    (query === "" ? "" : "?" + query);
 
-    const response = await fetch(targetURL, {
-        method: c.req.method,
-        headers: c.req.raw.headers,
-        body: c.req.method === "GET" || c.req.method === "HEAD" ? undefined : c.req.raw.body,
-    });
+  const response = await fetch(targetURL, {
+    method: c.req.method,
+    headers: c.req.raw.headers,
+    body: c.req.method === "GET" || c.req.method === "HEAD"
+      ? undefined
+      : c.req.raw.body,
+  });
 
-    return new Response(response.body, {
-        status: response.status,
-        headers: Object.fromEntries(
-            [
-                ...response.headers.entries(),
-                ["Access-Control-Allow-Origin", "*"]
-            ]
-        ),
-        statusText: response.statusText,
-    })
-})
+  return new Response(response.body, {
+    status: response.status,
+    headers: Object.fromEntries(
+      [
+        ...response.headers.entries(),
+        ["Access-Control-Allow-Origin", "*"],
+      ],
+    ),
+    statusText: response.statusText,
+  });
+});
 
 app.all("/_proxy/CHROME_GW/*", async (c) => {
-    const query = (new URL(c.req.url).searchParams.toString());
-    const targetURL = "https://line-chrome-gw.line-apps.com" + c.req.path.replace("/_proxy/CHROME_GW", "") + (query === "" ? "" : "?" + query);
+  const query = new URL(c.req.url).searchParams.toString();
+  const targetURL = "https://line-chrome-gw.line-apps.com" +
+    c.req.path.replace("/_proxy/CHROME_GW", "") +
+    (query === "" ? "" : "?" + query);
 
-    const response = await fetch(targetURL, {
-        method: c.req.method,
-        headers: c.req.raw.headers,
-        body: c.req.method === "GET" || c.req.method === "HEAD" ? undefined : c.req.raw.body,
-    });
+  const response = await fetch(targetURL, {
+    method: c.req.method,
+    headers: c.req.raw.headers,
+    body: c.req.method === "GET" || c.req.method === "HEAD"
+      ? undefined
+      : c.req.raw.body,
+  });
 
-    return new Response(response.body, {
-        status: response.status,
-        headers: Object.fromEntries(
-            [
-                ...response.headers.entries(),
-                ["Access-Control-Allow-Origin", "*"]
-            ]
-        ),
-        statusText: response.statusText,
-    })
-})
+  return new Response(response.body, {
+    status: response.status,
+    headers: Object.fromEntries(
+      [
+        ...response.headers.entries(),
+        ["Access-Control-Allow-Origin", "*"],
+      ],
+    ),
+    statusText: response.statusText,
+  });
+});
 
-app.use("*", serveStatic({
+app.use(
+  "*",
+  serveStatic({
     root: "./www",
-    onNotFound(path, c) {
-      return c.redirect("/?fallbackBy=" + encodeURIComponent(path));
-    }
-}));
+  }),
+);
 
-const options = Deno.args[0] === "localhost" ? { 
+app.notFound((c) =>
+  c.redirect("/?fallbackBy=" + encodeURIComponent(c.req.path))
+);
+
+const options = Deno.args[0] === "localhost"
+  ? {
     cert: await Deno.readTextFile("./secret/cert.pem"),
     key: await Deno.readTextFile("./secret/key.pem"),
-} : {}
+  }
+  : {};
 
 Deno.serve({
-    port: 443,
-    ...options
-}, app.fetch)
+  port: 443,
+  ...options,
+}, app.fetch);
